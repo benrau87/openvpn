1e8e1ba5aae54e9951f4befab48c504b1ce885d1
benrau87 <benrau87@users.noreply.github.com>
2016-11-03 09:32:35 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-wget http://swupdate.openvpn.org/as/openvpn-as-2.0.10-Ubuntu14.amd_64.deb
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
 
+wget http://swupdate.openvpn.org/as/openvpn-as-2.0.10-Ubuntu14.amd_64.deb
+dpkg -i openvpn*
+
+echo "Please enter a new password for openvpn"
+passwd openvpn
+
+echo
+echo "Please navigate to https://$HOSTNAME:943/admin to setup your VPN" 
