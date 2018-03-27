45967c1bd3c55a1bc21ab4c225258063d5c92bef
benrau87 <benrau87@users.noreply.github.com>
2018-03-27 11:32:54 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ if [ "$EUID" -ne 0 ]
   exit 1
 fi
 
-wget http://swupdate.openvpn.org/as/openvpn-as-2.0.10-Ubuntu14.amd_64.deb
+#wget http://swupdate.openvpn.org/as/openvpn-as-2.0.10-Ubuntu14.amd_64.deb
+wget http://swupdate.openvpn.org/as/openvpn-as-2.5-Debian9.amd_64.deb
 dpkg -i openvpn-as*
 
 echo "Please enter a new password for openvpn"
